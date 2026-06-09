2ce122de859b8d8b5f9ce986723da5608caa3135
Evelyn333444 <brackesb12@gmail.com>
2026-06-09 11:59:55 -0500

    first push

---
+++ skinstric/src/pages/home/home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function home() {
+  return (
+    <div>
+      <header className="app-header">
+        <div className="header-left">
+          <span className="header-title">SKINSTRIC</span>
+          <span className="header-intro">[INTRO]</span>
+        </div>
+        <div className="header-right">
+          <button className="enter-code-btn">ENTER CODE</button>
+        </div>
+      </header>
+      <body>
+        <div className="body-left">
+
+        </div>
+        <div className="body-content">
+            <div className= "center-text">Sophisticated Skincare</div>
+        </div>
+        <div className="body-right">
+            
+        </div>
+      </body>
+    </div>
+  )
+}
+
+export default home
