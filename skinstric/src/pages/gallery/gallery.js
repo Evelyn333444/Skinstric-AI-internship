7d9dde5e617026bbcf26c313fde2c78dba4331e4
Evelyn333444 <brackesb12@gmail.com>
2026-06-10 11:38:33 -0500

    first push

--- skinstric/src/pages/gallery/gallery.js
+++ skinstric/src/pages/gallery/gallery.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import './gallery.css'
 import BackButton from './assets/backbutton.svg'
 import ScanFace from './assets/scanFace.svg'
 import AccessGallery from './assets/accessGallery.svg'
 
-function gallery() {
-
-    const handleBackClick = () => {
+function gallery({ onBackClick }) {
+  const handleBackClick = () => {
     if (onBackClick) {
       onBackClick();
     }
@@ -22,6 +22,30 @@ function gallery() {
       <main>
         <div className="body-left-intro">
           <span className="intro-text">TO START ANALYSIS</span>
+        </div>
+        <div className="body-center">
+            <div className="gallery-card">
+              <div className="gallery-romb1">
+                <div className="gallery-romb2">
+                  <div className="gallery-romb3">
+                    <div className="gallery-image-wrapper">
+                      <img src={ScanFace} alt="Scan Your Face" className="gallery-image" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="gallery-card">
+              <div className="gallery-romb1">
+                <div className="gallery-romb2">
+                  <div className="gallery-romb3">
+                    <div className="gallery-image-wrapper">
+                      <img src={AccessGallery} alt="Access Gallery" className="gallery-image" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
       </main>
       <footer>
