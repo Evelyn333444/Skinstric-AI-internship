ce2da03650254a284bd66898f4d906471c062574
Evelyn333444 <brackesb12@gmail.com>
2026-06-10 12:07:20 -0500

    first push

---
+++ skinstric/src/pages/prepareAnalysis/prepareAnalysis.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './prepareAnalysis.css'
+
+function prepareAnalysis() {
+  return (
+    <div className="body-context">
+      <div className="prepare-square-1">
+        <div className="prepare-square-2">
+          <div className="prepare-square-3">
+            <span className="body-text">PREPARING YOUR ANALYSIS...</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default prepareAnalysis
