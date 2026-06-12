ecc71a470df2e95dde6ae5421733853be9b96ec5
Evelyn333444 <brackesb12@gmail.com>
2026-06-12 09:38:00 -0500

    first push

---
+++ skinstric/src/pages/settingUpCamera/settingUpCamera.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import SetUpCamera from './assets/setupcamera.svg'
+import SetUpCameraBetterResults from './assets/setUpCameraBetterResults.svg'
+
+function settingUpCamera() {
+
+    const [hoverRombText, setHoverRombText] = useState(false);
+
+  return (
+    <div>
+      <main>
+        <div className="rombuses">
+        {hoverRombText ? (
+        <div className="romb1">
+        <div className="romb2">
+        <div className="romb3">
+        <div className="set-up-camera-icon">
+            <img src={SetUpCamera} alt=""></img>
+            onMouseEnter={() => setHoverRombText(true)}
+            onMouseLeave={() => setHoverRombText(false)}
+        </div>
+        </div>
+        </div>
+        </div>
+        )}
+        </div>
+        <div className="text">
+            <img src={SetUpCameraBetterResults} alt=""></img>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default settingUpCamera
